f0ab67305239cbba7926d43baf4616c3884264ec
Tom <tomascollins@gmail.com>
2024-10-12 19:13:40 +0100

    add habit

--- resources/views/livewire/habit-list.blade.php
+++ resources/views/livewire/habit-list.blade.php
@@ -1,30 +1,56 @@
-namespace App\Http\Livewire;
+<?php
 
-use Livewire\Component;
 use App\Models\Habit;
 
-class HabitList extends Component
-{
-public $habits;
+use function Livewire\Volt\mount;
+use function Livewire\Volt\on;
+use function Livewire\Volt\state;
 
-protected $listeners = ['habitAdded' => '$refresh'];
+state(['habits' => []]);
 
-public function mount()
-{
-$this->habits = Habit::all();
-}
+mount(function () {
+    $this->habits = Habit::all();
+});
 
-public function deleteHabit($id)
-{
-$habit = Habit::find($id);
-if ($habit) {
-$habit->delete();
-$this->habits = Habit::all();
-}
-}
+on(['habitAdded' => fn() => ($this->habits = Habit::all())]);
 
-public function render()
-{
-return view('livewire.habit-list');
-}
-}
+$deleteHabit = function ($id) {
+    $habit = Habit::find($id);
+    if ($habit) {
+        $habit->delete();
+        $this->habits = Habit::all();
+    }
+};
+
+?>
+
+<div>
+    <h3 class="mb-4 text-lg font-semibold">Your Habits</h3>
+    <ul class="space-y-2">
+        @foreach ($habits as $habit)
+            <li class="flex items-center justify-between">
+                <a href="{{ route('habit.show', $habit->slug) }}" class="text-blue-600 hover:underline">
+                    {{ $habit->name }}
+                </a>
+                <button wire:click="$emit('confirmDelete', {{ $habit->id }})" class="text-red-600 hover:text-red-800">
+                    Delete
+                </button>
+            </li>
+        @endforeach
+    </ul>
+    <div class="mt-4">
+        <livewire:habit-form />
+    </div>
+
+    <script>
+        window.addEventListener('livewire:load', function() {
+            Livewire.on('confirmDelete', habitId => {
+                if (confirm(
+                        'Are you sure you want to delete this habit? All associated sessions will be removed.'
+                    )) {
+                    @this.call('deleteHabit', habitId);
+                }
+            });
+        });
+    </script>
+</div>
